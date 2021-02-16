4607aa895fcd6c87041670bee75ec6da7a4ea7a2
WanGak Kim <kimgak22@gmail.com>
2021-02-16 13:01:04 +0900

    Initial Commit

---
+++ umanager_hybrid/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  umanager_hybrid
+//
+//  Created by WanGakKim on 2021/02/16.
+//  
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ umanager_hybridTests/umanager_hybridTests.swift
@@ -0,0 +1,35 @@
+//
+//  umanager_hybridTests.swift
+//  umanager_hybridTests
+//
+//  Created by WanGakKim on 2021/02/16.
+//  
+//
+
+
+import XCTest
+@testable import umanager_hybrid
+
+class umanager_hybridTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
